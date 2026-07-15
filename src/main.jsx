22f36b3ebad9af5119ae07bbfbf9898bc0199dc9
Zahid Chauhan <your-email@example.com>
2026-07-15 15:40:44 +0500

    Fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Agar file ka naam Capital A hai to yeh chalega
+import App from './App.jsx'
 import './index.css'
-
-// Dono cases ko handle karne ke liye fallback check
-import * as SmallApp from './app.jsx' 
-
-const MainComponent = App || SmallApp.default;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainComponent />
+    <App />
   </React.StrictMode>,
 )
